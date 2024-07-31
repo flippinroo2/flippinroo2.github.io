4f9902748f176a02f810428539d1d810b658bc8b
flippinroo2 <flippinroo2@gmx.com>
2024-07-31 17:57:00 -0400

    - Adding initial content from someone else's static website (WILL BE DELETING ALL THE CONTENT LATER, THIS IS JUST PLACEHOLDER STUFF... AND WILL NOT BE USED)

---
+++ js/swiperstart.js
@@ -0,0 +1,19 @@
+  var mySwiper = new Swiper('.swiper-container', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+
+    // If we need pagination
+    pagination: '.swiper-pagination',
+
+    // Navigation arrows
+    nextButton: '.swiper-button-next',
+    prevButton: '.swiper-button-prev',
+    autoplay: 4000,
+    autoplayDisableOnInteraction: false,
+
+    paginationClickable: true,
+    // And if we need scrollbar
+
+  })
+  
